dd362cec9fbd4fa98cb5b4be2a45964256d6f202
MACK Mathieu <mathieu.mack@LIVE.FR>
2016-09-26 21:57:32 +0200

    First version of the Reader extension.

---
+++ SQLite-Net.Extensions.Readers/SQLite-Net.Extensions.Readers/ReaderItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SQLite_Net.Extensions.Readers
+{
+    public class ReaderItem
+    {
+        private readonly IDictionary<string, object> data;
+
+        public object this[string propertyName]
+        {
+            get
+            {
+                if (data.ContainsKey(propertyName))
+                    return data[propertyName];
+                else
+                    return null;
+            }
+            set
+            {
+                if (data.ContainsKey(propertyName))
+                    data[propertyName] = value;
+                else
+                    data.Add(propertyName, value);
+            }
+        }
+
+        /// <summary>
+        /// Get column names
+        /// </summary>
+        public List<string> Fields
+        {
+            get
+            {
+                return data.Keys.ToList();
+            }
+        }
+
+        public ReaderItem()
+        {
+            data = new Dictionary<string, object>();
+        }
+    }
+}
---
+++ nuGet/packageGenerator.ps1
@@ -0,0 +1,24 @@
+$location  = Get-Location
+
+$location.Path
+
+$strPath = $location.Path + '\..\SQLite-Net.Extensions.Readers\SQLite-Net.Extensions.Readers\bin\Release\SQLite-Net.Extensions.Readers.dll'
+
+$strPath
+
+$Assembly = [Reflection.Assembly]::Loadfile($strPath)
+$AssemblyName = $Assembly.GetName()
+$Assemblyversion =  $AssemblyName.version
+$Assemblyversion
+
+$nuSpecFile =  $location.Path + '\SQLite-Net.Extensions.Readers.nuspec'
+
+(Get-Content $nuSpecFile) | 
+Foreach-Object {$_ -replace "(<version>([0-9.]+)<\/version>)", "<version>$Assemblyversion</version>" } | 
+Set-Content $nuSpecFile
+
+.\NuGet pack SQLite-Net.Extensions.Readers.nuspec
+
+$apiKey = Read-Host 'Please set apiKey to publish to nuGet :'
+	
+.\NuGet push SQLite-Net.Extensions.Readers.$Assemblyversion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey

142a057f4462ec7a38c0b04966ab5ba300ac51b6
Mathieu MACK <mathieu.mack@live.fr>
2017-01-16 11:45:41 +0100

    Update CI configuration

--- nuspec/packageGenerator.ps1
+++ nuspec/packageGenerator.ps1
@@ -1,4 +1,7 @@
-#$location  = "C:\Sources\NoSqlRepositories";
+write-host "**************************" -foreground "Cyan"
+write-host "*   Packaging to nuget   *" -foreground "Cyan"
+write-host "**************************" -foreground "Cyan"
+
 $location  = $env:APPVEYOR_BUILD_FOLDER
 
 $locationNuspec = $location + "\nuspec"
@@ -13,18 +16,27 @@ $strPath = $location + '\SQLite-Net.Extensions.Readers\bin\Release\SQLite-Net.Ex
 
 $VersionInfos = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($strPath)
 $ProductVersion = $VersionInfos.ProductVersion
-"Product version : " + $ProductVersion
+"Product version : " + $ProductVersion -foreground "Green"
 
-"Update nuspec versions ..."	
+write-host "Product version : " $ProductVersion -foreground "Green"
+
+write-host "Packaging to nuget..." -foreground "Magenta"
+
+write-host "Update nuspec versions" -foreground "Green"
+
+write-host "Update nuspec versions SQLite-Net.Extensions.Readers.nuspec" -foreground "DarkGray"
 $nuSpecFile =  $locationNuspec + '\SQLite-Net.Extensions.Readers.nuspec'
 (Get-Content $nuSpecFile) | 
 Foreach-Object {$_ -replace "{BuildNumberVersion}", "$ProductVersion" } | 
 Set-Content $nuSpecFile
 
-"Generate nuget package ..."
+write-host "Generate nuget packages" -foreground "Green"
+
 .\NuGet.exe pack SQLite-Net.Extensions.Readers.nuspec
 
 $apiKey = $env:NuGetApiKey
 	
-#"Publish packages ..."	
+write-host "Publish nuget packages" -foreground "Green"
+
+write-host SQLite-Net.Extensions.Readers.$ProductVersion.nupkg -foreground "DarkGray"
 .\NuGet push SQLite-Net.Extensions.Readers.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
